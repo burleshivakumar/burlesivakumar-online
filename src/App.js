d32a81f48a2cfef03a3206454b5e13edf5eab6ec
burle sivakumar <burleshivakumar@gmail.com>
2021-08-29 18:20:59 +0530

    Added Sub Titles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Sheet from './UI Components/Cards and Sections/Sheet';
+import Card from './UI Components/Cards and Sections/Card';
+import { RESUME_SUB_TITLES }  from './assets/resumeDetails';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>
-          Siva Kumar Burle
-        </h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React Reusable UI components
-        </a>
-      </header>
-    </div>
-  );
-}
 
+const App = () => (
+
+  RESUME_SUB_TITLES.map((item) => (
+    <Sheet
+    headerText={item}
+  >
+    <Card>
+      Content here
+    </Card>
+  </Sheet>
+  ))
+  
+)
 export default App;
